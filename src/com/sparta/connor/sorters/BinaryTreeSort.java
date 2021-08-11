1a76108246c0e5e0eb1942c35b7e34a1be265090
C-Platts <cplatts@spartaglobal.com>
2021-08-11 16:44:08 +0100

    Added Class to Implement Binary Tree Sort. BinaryTreeSort imlpements Sortable.

---
+++ src/com/sparta/connor/sorters/BinaryTreeSort.java
@@ -0,0 +1,8 @@
+package com.sparta.connor.sorters;
+
+public class BinaryTreeSort implements Sorter{
+    @Override
+    public int[] sortArray(int[] arrayToSort) {
+        return new int[0];
+    }
+}
