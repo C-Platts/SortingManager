6a7f86af248bec83c174f322b8f95752511fabfb
C-Platts <cplatts@spartaglobal.com>
2021-08-12 12:20:37 +0100

    Initialised test cases for NodeTest class

---
+++ src/test/java/NodeTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+public class NodeTest {
+
+    @Test
+    public void testConstructorInitialisesValue() {
+
+    }
+
+    @Test
+    public void testGetLeftChildReturnsNode() {
+
+    }
+
+    @Test
+    public void testGetLeftChildReturnsNull() {
+
+    }
+
+    @Test
+    public void testGetRightChildReturnsNode() {
+
+    }
+
+    @Test
+    public void testGetRightChildReturnsNull() {
+
+    }
+
+    @Test
+    public void testGetValueReturnsFive() {
+
+    }
+
+    @Test
+    public void testIsLeftChildEmptyReturnsTrue() {
+
+    }
+
+    @Test
+    public void testIsLeftChildEmptyReturnsFalse() {
+
+    }
+
+    @Test
+    public void testIsRightChildEmptyReturnsTrue() {
+
+    }
+
+    @Test
+    public void testIsRightChildEmptyReturnsFalse() {
+
+    }
+
+
+}
