788c1ee894144db5853d1cc1c514da7ef716f446
CPlatts <cplatts@spartaglobal.com>
2021-08-13 14:54:21 +0100

    SelectionSort passes all tests

--- src/main/java/com/sparta/connor/sorters/SelectionSort.java
+++ src/main/java/com/sparta/connor/sorters/SelectionSort.java
@@ -1,8 +1,30 @@
 package com.sparta.connor.sorters;
 
+import com.sparta.connor.util.SorterUtil;
+
 public class SelectionSort implements Sorter{
     @Override
     public int[] sortArray(int[] arrayToSort) {
-        return new int[0];
+        return selectionSort(arrayToSort);
+    }
+
+    private int[] selectionSort(int[] arrayToSort) {
+
+        for(int i = 0; i < arrayToSort.length - 1; i ++) {
+
+            int sortedIndex = i;
+
+            for(int j = i + 1; j < arrayToSort.length; j ++) {
+                if(arrayToSort[j] < arrayToSort[sortedIndex]) {
+                    sortedIndex = j;
+                }
+            }
+
+            int temp = arrayToSort[sortedIndex];
+            arrayToSort[sortedIndex] = arrayToSort[i];
+            arrayToSort[i] = temp;
+        }
+
+        return arrayToSort;
     }
 }
