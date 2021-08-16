940e4d3981c52d8f02dd0f8223c5c08095428e22
CPlatts <cplatts@spartaglobal.com>
2021-08-16 17:24:59 +0100

    Added performance tests which can compare speeds of the implemented sorting algorithms.

--- src/test/java/performance/PerformanceTests.java
+++ src/test/java/performance/PerformanceTests.java
@@ -18,65 +18,63 @@ import java.util.logging.Logger;
 public class PerformanceTests {
 
     private static ArrayGenerator arrayGenerator;
-    private static int[] array1, array2;
+    private static Sorter sorters[];
+    private static int[][] arrays;
 
     private static final int arrayLength = 1000;
 
-    private static Logger logger = Logger.getLogger("SortManager");
+    private static final Logger logger = Logger.getLogger("SortManager");
 
     @BeforeAll
     static void setup() {
         arrayGenerator = new ArrayGenerator();
 
-        //two arrays of the same length
-        array1 = arrayGenerator.generate(arrayLength);
-        array2 = arrayGenerator.generate(arrayLength);
+        sorters = new Sorter[6];
+        arrays = new int[6][];
+
+        //Get an array of each sorter type
+        //Initialise the same number of arrays to sort
+        for(int i = 1; i <= 6; i ++) {
+            sorters[i - 1] = SorterFactory.getSorter(i);
+            arrays[i - 1] = arrayGenerator.generate(1000);
+        }
     }
 
     @Test
-    public void testMergeSortIsFasterThanJavaSort() {
-        double javaSortTime;
-        double arraySortTime;
+    public void testSorthingSpeeds() {
 
-        Sorter sorter = SorterFactory.getSorter(2);
+        double[] results = getTimes(sorters, arrays);
 
-        double[] results = getTimes(sorter);
-
-        Assertions.assertTrue(results[0] < results[1]);
-
-    }
-
-    @Test
-    public void testBubbleSortIsSlowerThanJavaSort() {
+        //merge sort should be faster than bubble
+        Assertions.assertTrue(results[1] < results[0]);
 
 
-        Sorter sorter = SorterFactory.getSorter(1);
-
-        double[] results = getTimes(sorter);
-
-        Assertions.assertFalse(results[0] < results[1]);
 
     }
 
-    private double[] getTimes(Sorter sorter) {
-        double javaSortTime;
-        double arraySortTime;
-
+    private double getTime(Sorter sorter, int[] array) {
         double start = System.nanoTime();
-        sorter.sortArray(array1);
+        sorter.sortArray(array);
         double end = System.nanoTime();
+        return end - start;
+    }
 
-        arraySortTime = end - start;
+    private double[] getTimes(Sorter[] sorters, int[][] arrays) {
+        double[] times = new double[6];
 
-        start = System.nanoTime();
-        Arrays.sort(array2);
-        end = System.nanoTime();
-        javaSortTime = end - start;
+        for(int i = 0; i < 6; i ++) {
+            times[i] = getTime(sorters[i], arrays[i]);
+        }
 
-        logger.info("Array sort time: " + arraySortTime);
-        logger.info("Java sort time: " + javaSortTime);
+        logger.info("Bubble sort time: " + times[0]);
+        logger.info("Merge sort time: " + times[1]);
+        logger.info("Binary Tree sort time: " + times[2]);
+        logger.info("Quick time: " + times[3]);
+        logger.info("Insertion sort time: " + times[4]);
+        logger.info("Selection sort time: " + times[5]);
 
-        return new double[]{arraySortTime, javaSortTime};
+
+        return times;
     }
 
 
